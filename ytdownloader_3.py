734ed03ab381e8104ac49bfdf5e0fdf21cff4db4
abhishekupadhyay260 <47971436+abhishekupadhyay260@users.noreply.github.com>
2021-08-06 23:54:52 +0530

    Add files via upload

---
+++ ytdownloader_3.py
@@ -0,0 +1,14 @@
+from pytube import YouTube
+YouTube_link = input("Enter the link")
+my_video = YouTube(YouTube_link)
+print("downloading...")
+# my_video = my_video.streams.get_highest_resolution
+# my_video.download()
+my_video = my_video.streams.get_highest_resolution()
+
+#or
+#my_video = my_video.streams.first()
+
+#Download video
+my_video.download()
+print("video downloaded successfully")
