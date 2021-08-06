15a6a043c451b612aa9fea4242787e89375da046
abhishekupadhyay260 <47971436+abhishekupadhyay260@users.noreply.github.com>
2021-08-07 01:20:55 +0530

    Update ytdownloader_3.py

--- ytdownloader_3.py
+++ ytdownloader_3.py
@@ -1,14 +1,8 @@
 from pytube import YouTube
 YouTube_link = input("Enter the link")
 my_video = YouTube(YouTube_link)
-print("downloading...")
-# my_video = my_video.streams.get_highest_resolution
-# my_video.download()
+print("downloading...") #not must in program i have written so that when run program looks cool, as it shows file is downloading
 my_video = my_video.streams.get_highest_resolution()
-
-#or
-#my_video = my_video.streams.first()
-
-#Download video
+# automatically download video in highest resolution
 my_video.download()
 print("video downloaded successfully")
